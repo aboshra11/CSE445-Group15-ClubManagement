21f7d00a02b3e7b27a073ce45727221840af4269
rarelavender007 <140261528+rarelavender007@users.noreply.github.com>
2026-04-12 17:36:13 -0700

    Add files via upload

---
+++ ClubManagementWeb/CaptchaControl.ascx
@@ -0,0 +1,15 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CaptchaControl.ascx.cs" 
+            Inherits="ClubManagementWeb.CaptchaControl" %>
+<div style="background:#f0f0f0; padding:15px; border:1px solid #ccc; width:300px;">
+    <div style="text-align:center; margin-bottom:10px;">
+        <asp:Label ID="lblCaptcha" runat="server" Font-Bold="true" Font-Size="24px" 
+                   BackColor="Yellow" ForeColor="Blue" Font-Names="Courier New" />
+    </div>
+    <div>
+        <asp:TextBox ID="txtCaptcha" runat="server" placeholder="Enter the code above" Width="200px" />
+        <asp:Button ID="btnRefresh" runat="server" Text="Refresh" OnClick="btnRefresh_Click" CausesValidation="false" />
+    </div>
+    <div>
+        <asp:Label ID="lblError" runat="server" ForeColor="Red" Visible="false" />
+    </div>
+</div>
---
+++ ClubManagementWeb/CaptchaControl.ascx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web.UI;
+
+namespace ClubManagementWeb
+{
+    public partial class CaptchaControl : UserControl
+    {
+        private string actualCaptcha;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GenerateCaptcha();
+            }
+        }
+
+        private void GenerateCaptcha()
+        {
+            Random rand = new Random();
+            actualCaptcha = rand.Next(1000, 9999).ToString();
+            lblCaptcha.Text = actualCaptcha;
+        }
+
+        protected void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GenerateCaptcha();
+            txtCaptcha.Text = "";
+            lblError.Visible = false;
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                if (txtCaptcha.Text == actualCaptcha)
+                {
+                    return true;
+                }
+                else
+                {
+                    lblError.Text = "Invalid CAPTCHA code! Please try again.";
+                    lblError.Visible = true;
+                    GenerateCaptcha();
+                    txtCaptcha.Text = "";
+                    return false;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            GenerateCaptcha();
+            txtCaptcha.Text = "";
+            lblError.Visible = false;
+        }
+    }
+}
